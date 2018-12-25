ba64423b5ca30b01674c1672724800faa0cc7078
Douglas Muraoka <douglas.muraoka@gmail.com>
2018-12-25 14:34:29 -0200

    docs: PostDetail component overview

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,3 +1,7 @@
+/**
+ * Component responsible for showing the details of a single post,
+ * as well as its comments.
+ */
 import React from 'react';
 
 export default ({ match }) => <div>Details of post: {match.params.id}</div>
